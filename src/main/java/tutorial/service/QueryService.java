22a4379756c55528e1516c82373851f50f4df24b
Karolina Zon <kzon@nexway.com>
2019-03-12 10:19:55 +0100

    step 4

---
+++ src/main/java/tutorial/service/QueryService.java
@@ -0,0 +1,12 @@
+package tutorial.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class QueryService {
+	
+	public String getHello() {
+		return "Hello world!";
+	}
+	
+}
--- src/main/java/tutorial/web/Query.java
+++ src/main/java/tutorial/web/Query.java
@@ -1,16 +1,22 @@
 package tutorial.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import tutorial.service.QueryService;
 
 @RestController
 @RequestMapping("/myapi")
 public class Query {
 	
+	@Autowired
+	QueryService queryService;
+	
 	@GetMapping(path = "/hello")
 	public String getHello() {
-		return "Hello world!";
+		return queryService.getHello();
 	}
 
 }
