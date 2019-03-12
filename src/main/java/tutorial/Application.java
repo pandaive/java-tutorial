43b269685d5b59c53b06ff0cb0f987fb3842a076
Karolina Zon <kzon@nexway.com>
2019-03-12 09:41:18 +0100

    step 2 solution

--- src/main/java/tutorial/Application.java
+++ src/main/java/tutorial/Application.java
@@ -1,9 +1,13 @@
 package tutorial;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		SpringApplication.run(Application.class, args);
 	}
 
 }
