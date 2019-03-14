99872b1f058a075c8d0f760b3b4ba8736e7c006b
Karolina Zon <kzon@nexway.com>
2019-03-14 15:04:59 +0100

    added logs

--- src/main/java/tutorial/web/Command.java
+++ src/main/java/tutorial/web/Command.java
@@ -6,11 +6,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
 import tutorial.model.Bill;
 import tutorial.service.CommandService;
 
 @RequestMapping("/myapi")
 @RestController
+@Slf4j
 public class Command {
 	
 	@Autowired
@@ -18,6 +20,7 @@ public class Command {
 	
 	@PostMapping(path = "/price", consumes = "application/json")
 	public double calculatePrice(@RequestBody Bill request) {
+		log.info("[Command] Received new calculatePrice request. Request body: {}", request);
 		return commandService.getPrice(request);
 	}
 }
